f5094d8fcb9093f67c975d39977a0d44f657ed7c
ro0opf <34717634+ro0opf@users.noreply.github.com>
2020-11-03 02:42:05 +0900

    Updates

---
+++ precache-manifest.1846657cf21752e46733a9f2c26373a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f7e47761a48741154b343d0ae0bcd7",
+    "url": "/who-is-faster/index.html"
+  },
+  {
+    "revision": "38d522f672961c7ca9de",
+    "url": "/who-is-faster/static/js/2.ffb3340a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/who-is-faster/static/js/2.ffb3340a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b60df2f3c666cecf2647",
+    "url": "/who-is-faster/static/js/main.8dd1e115.chunk.js"
+  },
+  {
+    "revision": "7e5f3de24d29cd5d5fc4",
+    "url": "/who-is-faster/static/js/runtime-main.461e6751.js"
+  }
+]);
