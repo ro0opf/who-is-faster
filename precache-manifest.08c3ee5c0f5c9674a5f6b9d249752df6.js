0e9baa2e52e10dc4d2f919cfea28565a714236f1
ro0opf <34717634+ro0opf@users.noreply.github.com>
2020-11-03 23:47:12 +0900

    Updates

---
+++ precache-manifest.08c3ee5c0f5c9674a5f6b9d249752df6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb585fd490ea998f7545775b2dd5891",
+    "url": "/who-is-faster/index.html"
+  },
+  {
+    "revision": "044b2ba310ed8f09eca4",
+    "url": "/who-is-faster/static/js/2.0af1f56b.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/who-is-faster/static/js/2.0af1f56b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae10bd0ccba6ddfe1c5",
+    "url": "/who-is-faster/static/js/main.b996457b.chunk.js"
+  },
+  {
+    "revision": "7e5f3de24d29cd5d5fc4",
+    "url": "/who-is-faster/static/js/runtime-main.461e6751.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/who-is-faster/precache-manifest.62d9e929ff0c4f89113852d4990c4e71.js"
+  "/who-is-faster/precache-manifest.08c3ee5c0f5c9674a5f6b9d249752df6.js"
 );
 
 self.addEventListener('message', (event) => {
